dda37276e116929d425109ea81b9d8be485d4eb2
MariyaPriya9 <mariyapriya.thanugundla@gmail.com>
2024-04-06 23:28:24 -0400

    first commit

---
+++ Lab 1 Rstudio.R
@@ -0,0 +1,104 @@
+# Print Hello world(Slide 22)
+print("Hello, World")
+
+
+#Get the output for the following values(Slide 23)
+A <- 5
+B <- 10
+#Calculating the values
+C <- A + B
+print(C)
+
+
+
+
+# Calculating Varaibles (Slide 24)
+v1 <- 4.5
+v2 <- 2
+v3 <- v1 / v2  
+v4 <- sqrt(v1)  
+print(v3)
+print(v4)
+
+
+
+# More Variables(Slide 25)
+greet <- "Hello,"
+name <- " Mariya"
+message <- paste(greet, name)
+print(message)
+
+
+
+# Try It Out(Slide 27)
+#Integers
+x <- 2L
+typeof(x)
+
+# Doubles
+y <- 2.5
+typeof(y)
+
+# Complex numbers
+z <- 2+3i
+typeof(z)
+
+# Characters
+a <- "h"
+typeof(a)
+
+# Logical
+q <- TRUE
+typeof(q)
+
+p <- FALSE
+typeof(p)
+
+
+#Basic Math(Slide 28)
+a <- 5
+b <- 3
+
+
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+
+# Six Operations (Slide 29)
+result <- 4 ^ 5
+print(result)
+typeof(result)
+
+
+# Basic Logic(Slide 30)
+result <- 4 < 5
+print(result)
+print(typeof(result))
+
+result2 <- !(5 > 1)
+print(result2)
+
+print(result & result2)
+print(result | result2)
+
+print(isTRUE(result))
+
+
+
+
+
+
+
+
+
